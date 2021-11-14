271858e8480278e735e99d6b77889291175c7d71
Danielle Meer <55391597+TIGERTIDES@users.noreply.github.com>
2021-11-14 11:55:32 +0800

    Profile Picture no refresh

--- PBL/Form1.cs
+++ PBL/Form1.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 
 namespace PBL
 {
@@ -15,7 +18,23 @@ namespace PBL
         public Form1()
         {
             InitializeComponent();
-
+            SqlConnection Wire = new SqlConnection("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = PBL; Integrated Security = True;");
+            Wire.Open();
+            SqlCommand get_picture = new SqlCommand("SELECT PROFILE_PICTURE FROM USER_DATA_PROFILE WHERE EMAIL ='" + Login.currentEmail + "';", Wire);
+            SqlDataAdapter SDA = new SqlDataAdapter(get_picture);
+            DataSet DS = new DataSet();
+            SDA.Fill(DS);
+            if (DS.Tables[0].Rows.Count > 0)
+            {
+                try
+                {
+                    MemoryStream MS = new MemoryStream((byte[])DS.Tables[0].Rows[0]["PROFILE_PICTURE"]);
+                    rButton1.BackgroundImage = new Bitmap(MS);
+                }
+                catch (System.InvalidCastException) { rButton1.BackgroundImage = new Bitmap("C:/Users/danielle meer/Documents/Programming_Projects/C#/School/PBL/PBL/bin/Images/profile_pic_placeholder.jfif"); }  // Go to PBL -> bin -> Images and click the address bar to replace the imagelocation
+                rButton1.BackgroundImageLayout = ImageLayout.Stretch;
+            }
+            Wire.Close();
         }
 
 
